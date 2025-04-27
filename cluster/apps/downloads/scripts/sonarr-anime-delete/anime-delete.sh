5a4368597314e1e59dba2b554f31ee306b3d3c05
Maxim Kurbatov <max@cubly.ru>
2025-04-28 02:44:15 +0500

    Add downloads

--- cluster/apps/downloads/scripts/sonarr-anime-delete/anime-delete.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-locations=(
-    "/media/Incoming/Usenet/Series"
-    "/media/Incoming/Torrents/Series"
-)
-
-find "${locations[@]}" -type f \( -iname "*op[0-9]*" -o -iname "*nced*" -o -iname "*ncop*" -o -iname "*music video*" \) -exec rm -rf {} \;
-
